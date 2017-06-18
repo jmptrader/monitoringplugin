545b8ade45f996728f33dd8672a9d006086ea77a
Johannes Drummer <j.drummer@thelambda.de>
2017-06-18 14:24:36 +0200

    test for percentage unit

--- perfdata_test.go
+++ perfdata_test.go
@@ -35,11 +35,21 @@ func ExamplePerformanceDataSpec() {
 			Minimum:           math.Inf(-1),
 			Maximum:           math.Inf(1),
 		},
+		{
+			Label:             "percenttest",
+			UnitOfMeasurement: PercentageUnitSpecification,
+			Minimum:           0.0,
+			Maximum:           100.0,
+		},
 	}
 
 	perfData := map[string]Unit{
 		"example": DurationUnit(40 * time.Second),
 		"counter": CounterUnit(60),
+		"percenttest": PercentageUnit{
+			Base:      100.0,
+			Quantitiy: 45.0,
+		},
 	}
 
 	for _, spec := range specs {
@@ -50,4 +60,5 @@ func ExamplePerformanceDataSpec() {
 	// 'example'=40s;80;90;0;
 	// 'counter'=60c;;;0;
 	// 'novalue'=UB;;;;
+	// 'percenttest'=45%;;;0;100
 }
